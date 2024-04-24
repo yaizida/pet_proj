95ebe8142d21bf46491dc3f6152f461a7eb9a246
izida <myizida@gmail.com>
2024-04-24 22:43:20 +0300

    start proj

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from schemas import Person
+
+app = FastAPI()
+
+
+@app.post('/hello')
+def greetings(person: Person) -> dict[str, str]:
+    if isinstance(person.surname, list):
+        surnames = ' '.join(person.surname)
+    else:
+        surnames = person.surname
+    result = ' '.join([person.name, surnames])
+    if person.age is not None:
+        result += ', ' + str(person.age)
+    if person.education_level is not None:
+        result += ', ' + person.education_level.lower()
+    if person.is_staff:
+        result += ', сотрудник'
+    return {'Hello': result}
---
+++ schemas.py
@@ -0,0 +1,20 @@
+# schemas.py
+
+from enum import Enum
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+
+class EducationLevel(str, Enum):
+    SECONDARY = 'Среднее образование'
+    SPECIAL = 'Среднее специальное образование'
+    HIGHER = 'Высшее образование'
+
+
+class Person(BaseModel):
+    name: str
+    surname: Union[str, list[str]]
+    age: Optional[int]
+    is_staff: bool = False
+    education_level: Optional[EducationLevel]
